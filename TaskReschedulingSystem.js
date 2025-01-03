5486f1a348f69b0ae0edb39e40eedee4eed7668f
Raghav Singh <147367154+RaghavSingh01@users.noreply.github.com>
2025-01-03 23:56:02 +0530

    Update TaskReschedulingSystem.js

--- TaskReschedulingSystem.js
+++ TaskReschedulingSystem.js
@@ -1,9 +1,29 @@
-let tasks = [ " Generate Idea ", " Study Idea ", " Gather Resources " , " Create Prototype ", " Publish Product "];
-console.log("Original Array: "+ tasks);
+let tasks = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"];
+console.log("Original Array: " + " " + tasks);
 
-tasks[4] = " Market the Product ";
-tasks[0] = "";
-tasks.unshift(" Find Good Opportunity ");
-tasks[1] = " Generate solution idea ";
+let tasks2 = [];
 
-console.log("Updated Array: "+ tasks);
+for(let i = 1; i < tasks.length; i++){
+  tasks2[i - 1] = tasks[i];
+
+}
+console.log(tasks2);
+let tasks3 = [];
+
+for(let i= 0; i < (tasks2.length + 2); i++){
+   
+    for(let j = 0; j < tasks2.length; j++){
+  if(i == 0){
+    tasks3[i] = "Priority task 1";
+    
+  }
+  else if( i == 1){
+    tasks3[i] = "Priority task 2";
+    
+  }
+  else{
+    tasks3[i] = tasks2[j];
+  }
+}
+}
+console.log(tasks3);
