1a8a61427220cc8e5c53064ace3fafd205a0137f
Raghav Singh <dhankarraghav13@gmail.com>
2025-01-09 11:42:19 +0530

    Sum of indexes changed

--- Sumofindexes.js
+++ Sumofindexes.js
@@ -1,12 +1,19 @@
-let arr = [[1,2],
-           [3,4],
-           [5,6]];
+let arr = [
+    [1,2],
+    [3,4],
+    [5,6]];
 
-let sum = "";
-for(let i = 0 ; i < arr.length; i++){
-    sum = "";
-    for(let j = 0; j < arr[i].length; j++){
-        sum = sum + " " +(i+j);
-        console.log(sum)
-    }
+let bag = "";
+    
+for(let i = 0; i < arr.length; i++){
+for(let j = 0; j < arr[i].length; j++){
+if(j == arr[i].length - 1){
+bag = bag + (i + j) +  "\n" ;
 }
+else{
+bag = bag + (i + j) + " ";
+}
+}
+}
+
+console.log(bag);
