1ae26775ee01c480c2cd608e64ff6f446606d36e
Raghav Singh <dhankarraghav13@gmail.com>
2025-01-10 23:59:41 +0530

    ff

--- Circulartraversal.js
+++ Circulartraversal.js
@@ -2,4 +2,26 @@ let arr = [[1,2,3],
            [4,5,6],
            [7,8,9]];
 
-for(let i = 0; i < arr.length; i++){}           
+
+let n = arr.length;
+let m = arr[0].length;
+let result = " "
+for(let i = n - 1; i >= 0; i--){
+    result += arr[i][0] + " ";
+}          
+console.log(result); 
+
+for(let j = 1; j < arr[0].length; j++){
+    result += arr[0][j] + " ";
+}
+console.log(result);
+for(let i = 1; i < n; i++ ){
+    result += arr[i][n-1] + " ";
+}
+console.log(result);
+
+for(let j = m - 1; j > n ; j--){
+    result += arr[n-1][j] + " ";
+}
+
+console.log(result);
