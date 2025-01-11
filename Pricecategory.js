c26c1f350804328585c251a4184f4974351be137
Raghav Singh <dhankarraghav13@gmail.com>
2025-01-11 12:32:29 +0530

    Price categorized based on multiple conditions

---
+++ Pricecategory.js
@@ -0,0 +1,5 @@
+let price = +(prompt("Enter the price."));
+
+let category = price >= 1000 ? "Expensive" : price >= 500 && price <= 999 ? "Moderate": price < 500 && price > 0 ? "Affordable": price == 0 ? "Free" : "Invalid Price";
+
+console.log(category)
