f2051d538d7c5a3a35bdbf799500f91c5725d793
Raghav Singh <dhankarraghav13@gmail.com>
2025-09-09 23:01:37 +0530

    Ci-cd added

--- Backend/Integration-testing/app.js
+++ Backend/Integration-testing/app.js
@@ -28,7 +28,7 @@ app.get('/health', (req, res) => {
 });
 
 // 404 handler
-app.use('*', (req, res) => {
+app.use( (req, res) => {
   res.status(404).json({
     status: 'error',
     message: `Route ${req.originalUrl} not found`
