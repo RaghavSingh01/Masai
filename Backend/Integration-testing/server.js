e1b75ffefc6fe6e082c4c5f67ece13b6af16eeee
Raghav Singh <dhankarraghav13@gmail.com>
2025-09-09 23:11:31 +0530

    error fixed

--- Backend/Integration-testing/server.js
+++ Backend/Integration-testing/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const connectDB = require('./config/database');
+const { connectDB } = require('./config/database');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
